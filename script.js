48082d49c37fcddd752daadcdc1997c4e8e38b42
Theelipan prabakar <theelipan@gmail.com>
2020-09-24 23:02:11 -0400

    add FAQ collapsible section

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const faqExpend = document.querySelectorAll(".faq-info__content-question");
-const faqClose = document.querySelectorAll(".faq-info__content-question");
-
-function hideQuestions() {
-  faqExpend.forEach((el) => {
-    console.log(el.nextSibling.nextSibling);
-    // el.nextSibling.nextSibling.style.height = "0";
-    // el.nextSibling.nextSibling.style.padding = "0";
-  });
-}
+const faqExpendBtn = document.querySelectorAll(".faq-info__content-btn");
 
 faqExpend.forEach((el) => {
   el.addEventListener("click", (e) => {
     showQuestion(e.target.nextElementSibling);
-    hideQuestions();
+    e.target.classList.add("hidden-pointer");
+  });
+});
+
+faqExpendBtn.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    showQuestion(el.parentElement.children[2]);
   });
 });
 
 function showQuestion(question) {
-  question.style.height = "auto";
-  question.style.padding = "20px 30px";
+  if (!question.classList.contains("faq-info__answer-show")) {
+    hideQuestions();
+    question.classList.add("faq-info__answer-show");
+  }
+}
+
+function hideQuestions() {
+  document.querySelectorAll(".faq-info__content-answer").forEach((el) => {
+    el.classList.remove("faq-info__answer-show");
+    el.parentElement.children[1].classList.remove("hidden-pointer");
+    el.parentElement.children[0].classList.remove("faq-info__content-btn-rotate");
+  });
 }
